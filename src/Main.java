224244f592f23c4b6a202c44c87f0419ec47dd37
Joshua Cruz <jcruz1998@gmail.com>
2024-11-02 18:55:57 -0400

    String Methods

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Josh";
+
+        //boolean result = name.equalsIgnoreCase("Josh");
+        //boolean result = name.isEmpty();
+
+        //int result = name.length();
+
+        //char result = name.charAt(0);
+
+        //int result = name.indexOf("J");
+
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+
+        //String result = name.trim();
+
+        String result = name.replace('o', 'a');
+
+        System.out.println(result);
+
+
+    }
+}
